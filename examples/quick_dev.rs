8211a40e436643f20de141c5394ea312c6c612a5
Jeremy Chone <jeremy.chone@gmail.com>
2023-07-14 16:06:26 +0200

    . add first basic tests and first quick dev

---
+++ examples/quick_dev.rs
@@ -0,0 +1,16 @@
+//! Note: http rest endpoints: https://sqa.stackexchange.com/questions/47097/free-sites-for-testing-post-rest-api-calls
+//!       Using: https://jsonplaceholder.typicode.com/
+
+use anyhow::Result;
+
+const END_POINT: &str = "https://jsonplaceholder.typicode.com";
+
+#[tokio::main]
+async fn main() -> Result<()> {
+	let hc = httpc_test::new_client(END_POINT)?;
+
+	let req = hc.do_get("/todos/1").await?;
+	req.print().await?;
+
+	Ok(())
+}
---
+++ tests/test_base.rs
@@ -0,0 +1,47 @@
+//! Note: http rest endpoints: https://sqa.stackexchange.com/questions/47097/free-sites-for-testing-post-rest-api-calls
+//!       Using: https://jsonplaceholder.typicode.com/
+
+use anyhow::Result;
+use serde_json::json;
+
+const TYPICODE_END_POINT: &str = "https://jsonplaceholder.typicode.com";
+
+#[tokio::test]
+async fn test_base_typicode_get() -> Result<()> {
+	let hc = httpc_test::new_client(TYPICODE_END_POINT)?;
+
+	let res = hc.do_get("/posts/1").await?;
+	let res = res.json_body()?.to_string();
+	assert!(res.contains(r#""body":"quia et suscipit\nsuscipit"#));
+	assert!(res.contains(r#""id":1"#));
+	assert!(res.contains(r#""userId":1"#));
+
+	Ok(())
+}
+
+#[tokio::test]
+async fn test_base_typicode_post() -> Result<()> {
+	let hc = httpc_test::new_client(TYPICODE_END_POINT)?;
+
+	let res = hc
+		.do_post(
+			"/posts",
+			json!(
+					{
+			  "userId": 1,
+			  "title": "test_base_typicode_post - title-01",
+			  "body": "test_base_typicode_post - body-01"
+			}
+				),
+		)
+		.await?;
+
+	let res = res.json_body()?;
+
+	assert_eq!(
+		res.to_string(),
+		r#"{"body":"test_base_typicode_post - body-01","id":101,"title":"test_base_typicode_post - title-01","userId":1}"#
+	);
+
+	Ok(())
+}
