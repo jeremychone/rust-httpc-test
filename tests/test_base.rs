0c939dbe7ffdf4e221835df1bb20c5f6c5fc268d
Jeremy Chone <jeremy.chone@gmail.com>
2023-07-14 17:36:40 +0200

    . update tests for res.status()

--- tests/test_base.rs
+++ tests/test_base.rs
@@ -8,10 +8,16 @@ const TYPICODE_END_POINT: &str = "https://jsonplaceholder.typicode.com";
 
 #[tokio::test]
 async fn test_base_typicode_get() -> Result<()> {
+	// -- Setup
 	let hc = httpc_test::new_client(TYPICODE_END_POINT)?;
 
+	// -- Exec
 	let res = hc.do_get("/posts/1").await?;
+	let status = res.status();
 	let res = res.json_body()?.to_string();
+
+	// -- Check
+	assert_eq!(status, 200);
 	assert!(res.contains(r#""body":"quia et suscipit\nsuscipit"#));
 	assert!(res.contains(r#""id":1"#));
 	assert!(res.contains(r#""userId":1"#));
@@ -21,8 +27,10 @@ async fn test_base_typicode_get() -> Result<()> {
 
 #[tokio::test]
 async fn test_base_typicode_post() -> Result<()> {
+	// -- Setup
 	let hc = httpc_test::new_client(TYPICODE_END_POINT)?;
 
+	// -- Exec
 	let res = hc
 		.do_post(
 			"/posts",
@@ -35,9 +43,11 @@ async fn test_base_typicode_post() -> Result<()> {
 				),
 		)
 		.await?;
-
+	let status = res.status();
 	let res = res.json_body()?;
 
+	// -- Check
+	assert_eq!(status, 201); // success, resource created.
 	assert_eq!(
 		res.to_string(),
 		r#"{"body":"test_base_typicode_post - body-01","id":101,"title":"test_base_typicode_post - title-01","userId":1}"#
